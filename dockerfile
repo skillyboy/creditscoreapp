a8cc477fffbef5126930d1496966f9e7e05272c2
Skillyboy <fakoredeabbas@gmail.com>
2024-02-17 17:29:42 +0100

    first commit

--- dockerfile
+++ dockerfile
@@ -1,8 +1,23 @@
-FROM python:3.11.4
-ENV PYTHONUNBUFFERED=1
-WORKDIR /project/creditapp
-COPY .requirements.txt /code/
-RUN pip install -r requirements.txt
+# Use an official Python runtime as a parent image
+FROM python:3.8
 
-# Run the Django server
+# Set environment variables
+ENV PYTHONUNBUFFERED 1
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the requirements file into the container
+COPY requirements.txt /app/
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the current directory contents into the container at /app/
+COPY . /app/
+
+# Expose the port your Django app will run on
+EXPOSE 8000
+
+# Define the command to run your application
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- project/settings.py
+++ project/settings.py
@@ -91,25 +91,26 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('skillysdb'),
-#         'USER': os.environ.get('skillyboy'),
-#         'PASSWORD': os.environ.get('skilly1234'),
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'creditapp_db',
+        'USER': 'creditapp_user',
+        'PASSWORD': 'secret_password',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
