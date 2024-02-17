72f89d0acbc380e6902647bb71fe8e90156d57d0
Skillyboy <fakoredeabbas@gmail.com>
2024-02-17 21:22:59 +0100

    Your commit message here

--- project/settings.py
+++ project/settings.py
@@ -104,12 +104,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("POSTGRES_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("POSTGRES_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("POSTGRES_USER", "user"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
-        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
-        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": os.environ.get("skillydb", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("skillyboy", "user"),
+        "PASSWORD": os.environ.get("skilly1234", "password"),
+        "HOST": '127.0.0.1',
+        'PORT': 5432,
+
     }
 }
 
